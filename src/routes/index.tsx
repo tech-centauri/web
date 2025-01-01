83613d62a68c679a8c2a26651491be15019ed57d
Aman Nambisan <aman.nambisan@atlan.com>
2025-01-01 17:09:52 +0530

    initial commit with tailwind, qwik and bun setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,22 +4,19 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      <h1 class="text-3xl">
+        Centauri
+      </h1>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "",
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: "name",
+      content: "description",
     },
   ],
 };
